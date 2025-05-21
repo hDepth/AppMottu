8ef5b52d07adac2e40d006e77594c09c0bde49d4
hDepth <dephenriquejp@gmail.com>
2025-05-21 02:31:03 -0300

    Redirection

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home">
+            <Stack.Navigator initialRouteName="Auth">
                 <Stack.Screen name="Auth" component={AuthScreen} options={{ headerShown: false }} />
                 <Stack.Screen
                     name="Home"
